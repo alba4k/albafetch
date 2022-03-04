d1ba6ba59f0de38a1cb1ee38dd313cafe96fa5ce
alba4k <blaskoazzolaaaron@gmail.com>
2022-03-04 17:42:13 +0100

    config.h

---
+++ config.h
@@ -0,0 +1,32 @@
+// CONFIGURATION OPTIONS:
+const static char spacing[5] = "    ";                      // defines the spacing between the logo and the infos
+const static char separator[20] = "------------------\n";   // defines what is used as separator between sections
+
+// add more logos if you want!
+const static char *logo[] = {
+    "                   -`                 ",
+    "                  .o+`                ",
+    "                 `ooo/                ",
+    "                `+oooo:               ",
+    "               `+oooooo:              ",
+    "               -+oooooo+:             ",
+    "             `/:-:++oooo+:            ",
+    "            `/++++/+++++++:           ",
+    "           `/++++++++++++++:          ",
+    "          `/+++ooooooooooooo/`        ",
+    "         ./ooosssso++osssssso+`       ",
+    "        .oossssso-````/ossssss+`      ",
+    "       -osssssso.      :ssssssso.     ",
+    "      :osssssss/        osssso+++.    ",
+    "     /ossssssss/        +ssssooo/-    ",
+    "   `/ossssso+/:-        -:/+osssso+-  ",
+    "  `+sso+:-`                 `.-/+oso: ",
+    " `++:.                           `-/+/",
+    " .`                                 `/"
+};
+
+// was just too lazy to do it right, so it's hard coded. fuck you <3
+const static char OS[18] = "Arch Linux";                    // idk how to implement it so get away with this
+const static char WM[3] = "i3";
+const static char shell[5] = "fish";
+const static char term[6] = "kitty";
