05c140c74abcb1436b9080d9371b288ec0ae1959
alba4k <blaskoazzolaaaron@gmail.com>
2022-03-07 12:12:14 +0100

    documentation

--- neofetch.c
+++ neofetch.c
@@ -9,7 +9,7 @@
 
 int main() {
 
-    struct sysinfo info;
+    struct sysinfo info;        // used to get memory, uptime
     sysinfo(&info);
 
     // ******** title ********
@@ -40,7 +40,7 @@ int main() {
 
     // ******** uptime ********
 
-    unsigned int secs = info.uptime;
+    unsigned int secs = info.uptime;            // total uptime in seconds
     unsigned int days = secs/86400;
     unsigned int hours = secs/3600 - days*24;
     unsigned int mins = secs/60 - days*864 - hours*60;
@@ -58,7 +58,7 @@ int main() {
         close(pipes[0]);
         dup2(pipes[1], STDOUT_FILENO);
 
-        execlp("uname", "uname", "-m", NULL);
+        execlp("uname", "uname", "-m", NULL);   // using "uname --machine" to get the cpu instruction set (x86_64?)
     } else {
         printf(COLOR "%s\e[0m" SPACING SEPARATOR COLOR "%s" SPACING "OS:\e[0m ", logo[3], logo[4]);
     }
@@ -78,7 +78,7 @@ int main() {
         close(pipes[0]);
         dup2(pipes[1], STDOUT_FILENO);
 
-        execlp("uname", "uname", "-r", NULL);
+        execlp("uname", "uname", "-r", NULL);   // using "uname --kernel-release" to get the kernel version
     } else {
         printf(COLOR "%s" SPACING "Kernel:\e[0m ", logo[5]);
     }
@@ -90,23 +90,17 @@ int main() {
     printf("%s\n", kernel);
     
     // ******** wm ********
-    printf(COLOR"%s" SPACING "Desktop:\e[0m %s\n", logo[6], getenv("XDG_CURRENT_DESKTOP"));
+    printf(COLOR"%s" SPACING "Desktop:\e[0m %s\n", logo[6], getenv("XDG_CURRENT_DESKTOP")); // $XDG_CURRENT_DESKTOP
 
     // ******** shell ********
 
-    printf(COLOR "%s" SPACING "Shell:\e[0m %s\n", logo[7], getenv("SHELL"));
+    printf(COLOR "%s" SPACING "Shell:\e[0m %s\n", logo[7], getenv("SHELL"));        // $SHELL
 
-    // ******** term ********
-    char *term = getenv("TERM");
-    if(term == "xterm-kitty") {
-        printf(COLOR "%s" SPACING "Terminal: \e[0m kitty\n", logo[8]);
-    } else if(term == "xterm-256color"){
-        printf(COLOR "%s" SPACING "Terminal: vscode\e[0m %s\n", logo[8]);
-    } else {
-        printf(COLOR "%s" SPACING "Terminal: \e[0m %s\n", logo[8], term);
-    }
+        // ******** term ********
+    printf(COLOR "%s" SPACING "Terminal: \e[0m %s\n", logo[8], getenv("TERM"));     // $TERM
+
     // ******** packages ********
-    // using pacman, only pacman, get away with it
+    // using pacman and only pacman, get away with it
 
     char packages[10];
 
@@ -115,8 +109,8 @@ int main() {
         close(pipes[0]);
         dup2(pipes[1], STDOUT_FILENO);
 
-        system("pacman -Q | wc -l");
-        exit(0);
+        system("pacman -Q | wc -l");        // using "pacman --query" to list the installed packages
+        exit(0);                            // using "wc --lines" to get the number of lines (wordcount)
     } else {
         printf(COLOR "%s" SPACING "Packages: ", logo[9]);
     }
@@ -143,17 +137,19 @@ int main() {
     // ******** mem ********
 
     unsigned long total = (info.totalram);
+    char used_str[14];
+
+    // would be way more elegant, but leaves a slight gap? idk, I could also use /proc/meminfo
     //unsigned long used = total - info.freeram - info.bufferram - info.sharedram;
 
     //printf(COLOR "%s" SPACING "Memory:\e[0m %lu MiB / %lu MiB (%lu%%)\n", logo[14], used/1048576, total/1048576, (used * 100) / (1048576*total));
-   char used_str[14];
     
     pipe(pipes);
     if(!fork()) {
         close(pipes[0]);
         dup2(pipes[1], STDOUT_FILENO);
 
-        system("free --byte | grep M | awk '{print $3}'");
+        system("free --byte | grep M | awk '{print $3}'");  // using free to count the used memory, 3rd arg
         exit(0);
     } else {
         printf(COLOR "%s" SPACING "Memory:\e[0m ", logo[14]);
