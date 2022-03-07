4b9723cc3544d8242fc603b96e463e6cd63815fa
alba4k <blaskoazzolaaaron@gmail.com>
2022-03-07 12:19:24 +0100

    documented config; renamed files

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 // CONFIGURATION OPTIONS:
 #define SPACING "    "                      // defines the distance between the logo and the infos
 #define SEPARATOR "------------------\n"    // defines what is used as separator between sections
-#define COLOR "\e[36m\e[1m"                      // defines the color to use
+#define COLOR "\e[36m\e[1m"                 // defines the color to use (ex: "\e[36m" would be light blue; "\e[1m" makes the text bold)
 
 // add more logos if you want, I couldn't care less
 const char *logo[] = {
@@ -27,8 +27,8 @@ const char *logo[] = {
 };
 
 // was just too lazy to do it right, so it's hard coded. fuck you <3
-#define OS "Arch Linux"
+#define OS "Arch Linux"                 // what will be printed as os
 
-#define HOST "HP ProBook 440 G6"
-#define CPU "i5-8265U"
-#define GPU "Intel UHD Graphics 620"
+#define HOST "HP ProBook 440 G6"        // what will be printed as host
+#define CPU "i5-8265U"                  // what will be printed as cpu. could get it from /proc/cpuinfo
+#define GPU "Intel UHD Graphics 620"    // what will be printed as gpu
