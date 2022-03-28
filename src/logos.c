296ea6cf380d1f0af0113a680590ec24d3cd532c
alba4k <blaskoazzolaaaron@gmail.com>
2022-03-28 19:06:49 +0200

    more logos

---
+++ src/logos.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+const char *archlinux[] = {
+    "                   -`                 ",
+    "                  .o+`                ",
+    "                 `ooo/                ",
+    "                `+oooo:               ",
+    "               `+oooooo:              ",
+    "               -+oooooo+:             ",
+    "             `/:-:++oooo+:            ",
+    "            `/++++/+++++++:           ",
+    "           `/++++++++++++++:          ",
+    "          `/+++ooooooooooooo/`        ",
+    "         ./ooosssso++osssssso+`       ",
+    "        .oossssso-````/ossssss+`      ",
+    "       -osssssso.      :ssssssso.     ",
+    "      :osssssss/        osssso+++.    ",
+    "     /ossssssss/        +ssssooo/-    ",
+    "   `/ossssso+/:-        -:/+osssso+-  ",
+    "  `+sso+:-`                 `.-/+oso: ",
+    " `++:.                           `-/+/",
+    " .`                                 `/"
+};
+
+const char *debian[] = {
+    "       _,met$$$$$gg.        ",
+    "    ,g$$$$$$$$$$$$$$$P.     ",
+    "  ,g$$P\"     \"\"\"Y$$.\".      ",
+    " ,$$P'              `$$$.   ",
+    "',$$P       ,ggs.     `$$b: ",
+    "`d$$'     ,$P\"'   .    $$$  ",
+    " $$P      d$'     ,    $$P  ",
+    " $$:      $$.   -    ,d$$'  ",
+    " $$;      Y$b._   _,d$P'    ",
+    " Y$$.    `.`\"Y$$$$P\"'       ",
+    " `$$b      \"-.__            ",
+    "  `Y$$                      ",
+    "   `Y$$.                    ",
+    "     `$$b.                  ",
+    "       `Y$$b.               ",
+    "          `\"Y$b._           ", 
+    "              `\"\"\"       "
+};
+int main() {
+for(int i = 0; i < (sizeof(debian)/sizeof(char*)); i++) {
+    printf("%s\n", debian[i]);
+}
+}
