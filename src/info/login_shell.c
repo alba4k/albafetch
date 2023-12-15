d796f79ccb35a7597234b3e0c2dc8cc48cafbf52
alba4k <blaskoazzolaaaron@gmail.com>
2023-12-15 20:00:59 +0100

    more missing includes for macos

--- src/info/login_shell.c
+++ src/info/login_shell.c
@@ -5,6 +5,10 @@
 
 #include <stdlib.h>
 
+#ifdef __APPLE__
+#include <libgen.h>
+#endif // __APPLE__
+
 // get the current login shell
 int login_shell(char *dest) {
     char *buf = getenv("SHELL");
