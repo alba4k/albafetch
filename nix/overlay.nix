e71d410619850b56d63130d019e4945c7292d79f
seth <getchoo@tuta.io>
2024-12-20 06:40:23 -0500

    nix cleanup (#134)
    * nix: use new darwin sdk pattern
    
    * nix: simplify flake
    
    • Removed input 'flake-compat'
    • Added input 'flake-utils':
        'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b?narHash=sha256-l0KFg5HjrsfsO/JpG%2Br7fRrqm12kzFHyUHqHCVpMMbI%3D' (2024-11-13)
    • Added input 'flake-utils/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e?narHash=sha256-Vy1rq5AaRuLzOxct8nz4T6wlgyUR7zLU309k9mBC768%3D' (2023-04-09)
    • Removed input 'parts'
    • Removed input 'parts/nixpkgs-lib'
    
    * nix: remove unused references from static build

--- nix/overlay.nix
+++
@@ -1,8 +0,0 @@
-{self, ...}: {
-  flake.overlays.default = final: prev: {
-    albafetch = prev.callPackage ./derivation.nix {
-      inherit (final.darwin.apple_sdk.frameworks) Foundation IOKit;
-      inherit self;
-    };
-  };
-}
---
+++ nix/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  stdenv,
+  meson,
+  ninja,
+  pciutils,
+  pkg-config,
+  sqlite,
+
+  self ? { },
+}:
+
+let
+  latestTag = "4.2.1";
+
+  fullDate = lib.substring 0 8 self.lastModifiedDate;
+
+  formattedDate =
+    if (self ? "lastModifiedDate") then
+      lib.concatStringsSep "-" [
+        # YYYY
+        (lib.substring 0 4 fullDate)
+        ## MM
+        (lib.substring 4 2 fullDate)
+        # DD
+        (lib.substring 6 2 fullDate)
+      ]
+    else
+      "unknown";
+in
+
+stdenv.mkDerivation {
+  name = "albafetch";
+  version = "${latestTag}-unstable-${formattedDate}";
+
+  src = self;
+
+  buildInputs = [
+    sqlite
+  ] ++ lib.optional stdenv.hostPlatform.isLinux pciutils;
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  OBJC = lib.optionalString stdenv.hostPlatform.isDarwin "clang";
+
+  postFixup = lib.optionalString stdenv.hostPlatform.isStatic ''
+    rm -r $out/nix-support
+  '';
+
+  meta = {
+    description = "Faster neofetch alternative, written in C.";
+    homepage = "https://github.com/alba4k/albafetch";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ getchoo ];
+    platforms = lib.platforms.unix;
+  };
+}
