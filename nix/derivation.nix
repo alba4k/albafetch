cade0f01cb683ed9a847ed2afc6ef51bb490fa0b
seth <getchoo@tuta.io>
2024-12-19 23:42:10 -0500

    nix: use new darwin sdk pattern

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -4,45 +4,53 @@
   meson,
   ninja,
   pciutils,
-  sqlite,
   pkg-config,
-  Foundation,
-  IOKit,
   self,
+  sqlite,
 }:
+
+let
+  latestTag = "4.2.1";
+
+  fullDate = lib.substring 0 8 self.lastModifiedDate;
+
+  formattedDate =
+    if (self ? "lastModifiedDate") then
+      lib.concatStringsSep "-" [
+        # YYYY
+        (lib.substring 0 4 fullDate)
+        ## MM
+        (lib.substring 4 2 fullDate)
+        # DD
+        (lib.substring 6 2 fullDate)
+      ]
+    else
+      "unknown";
+in
+
 stdenv.mkDerivation {
   name = "albafetch";
-  version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
+  version = "${latestTag}-unstable-${formattedDate}";
 
-  src = lib.cleanSource self;
+  src = self;
 
-  buildInputs =
-    [
-      sqlite.dev
-    ]
-    ++ lib.optional stdenv.isLinux pciutils;
+  buildInputs = [
+    sqlite
+  ] ++ lib.optional stdenv.hostPlatform.isLinux pciutils;
 
-  nativeBuildInputs =
-    [
-      meson
-      ninja
-      pkg-config
-    ]
-    ++ lib.optionals stdenv.isDarwin [
-      Foundation
-      IOKit
-    ];
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
 
-  OBJC =
-    if stdenv.isDarwin
-    then "clang"
-    else "";
+  OBJC = lib.optionalString stdenv.hostPlatform.isDarwin "clang";
 
-  meta = with lib; {
+  meta = {
     description = "Faster neofetch alternative, written in C.";
     homepage = "https://github.com/alba4k/albafetch";
-    license = licenses.mit;
-    maintainers = with maintainers; [getchoo];
-    platforms = platforms.unix;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ getchoo ];
+    platforms = lib.platforms.unix;
   };
 }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,7 +1,7 @@
-{self, ...}: {
+{ self, ... }:
+{
   flake.overlays.default = final: prev: {
     albafetch = prev.callPackage ./derivation.nix {
-      inherit (final.darwin.apple_sdk.frameworks) Foundation IOKit;
       inherit self;
     };
   };
