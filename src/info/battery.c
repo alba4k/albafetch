e639160592b5f7319389d9b29a31c524104b9706
alba4k <blaskoazzolaaaron@gmail.com>
2026-02-23 15:50:26 +0100

    macOS: battery percentage (no status)

--- src/info/battery.c
+++ src/info/battery.c
@@ -1,16 +1,20 @@
+#ifdef __APPLE__
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/ps/IOPowerSources.h>
+#include <IOKit/ps/IOPSKeys.h>
+#else
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-
 #ifdef __ANDROID__
 #include <ctype.h>
 #endif // __ANDROID__
+#endif // __APPLE__
 
 #include "info.h"
 #include "../config/config.h"
 #include "../utils/wrappers.h"
 
-// get the battery percentage and status (Linux only!)
 int battery(char *dest) {
     char capacity[5] = "";
     char status[20] = "";
@@ -49,6 +53,33 @@ int battery(char *dest) {
         }
     }
 #else
+#ifdef __APPLE__
+    CFTypeRef powerSourceInfo = IOPSCopyPowerSourcesInfo();
+    CFArrayRef powerSourcesList = IOPSCopyPowerSourcesList(powerSourceInfo);
+
+    if(CFArrayGetCount(powerSourcesList) == 0)
+        return ERR_NO_INFO;
+
+    CFDictionaryRef powerSource = CFArrayGetValueAtIndex(powerSourcesList, 0);
+
+    long current_capacity = 0;
+    long max_capacity = 0;
+
+    CFNumberRef currentRef = CFDictionaryGetValue(powerSource, CFSTR(kIOPSCurrentCapacityKey));
+    CFNumberRef maxRef = CFDictionaryGetValue(powerSource, CFSTR(kIOPSMaxCapacityKey));
+
+    if (currentRef && maxRef) {
+        CFNumberGetValue(currentRef, kCFNumberLongType, &current_capacity);
+        CFNumberGetValue(maxRef, kCFNumberLongType, &max_capacity);
+
+        snprintf(capacity, 5, "%ld", (current_capacity * 100) / max_capacity);
+    }
+    else
+        return ERR_GENERIC;
+
+    CFRelease(powerSourcesList);
+    CFRelease(powerSourceInfo);
+#else
     FILE *fp = NULL;
 
     if((fp = fopen("/sys/class/power_supply/BAT0/capacity", "r"))) {
@@ -69,6 +100,7 @@ int battery(char *dest) {
         else
             status[0] = 0;
     }
+#endif // __APPLE__
 #endif // __ANDROID__
 
     if(capacity[0] != 0 && status[0] != 0) {
